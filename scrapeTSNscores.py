d3588a96f80830064506036a7a08734c075fc8b2
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-10-07 08:01:47 -0400

    added the reference website to compare with TSN

--- scrapeTSNscores.py
+++ scrapeTSNscores.py
@@ -1,13 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-#get the data from the website
-data = requests.get('https://www.tsn.ca/')
+scoresWebsite = 'http://theapp/crsadmin/Default.aspx'
+#get the real scores
+scoresData = requests.get(scoresWebsite)
+
+print("The response from", scoresWebsite, "is", scoresData);
+print()
+
+TSNwebsite = 'https://www.tsn.ca/'
+#get the data displayed on TSN website
+TSNdata = requests.get(TSNwebsite)
+
+print("The response from", TSNwebsite, "is", TSNdata);
 
 #load data into bs4
-soup = BeautifulSoup(data.text, 'html.parser')
+soup = BeautifulSoup(TSNdata.text, 'html.parser')
 
 #get data simply by looking for each <tr>
-data =[]
+print("Getting data from", TSNwebsite)
+TSNdata =[]
 for widget in soup.find_all('widgets-bms-scores'):
     print(widget)
+
