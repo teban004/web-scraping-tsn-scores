333993568645629dfad63ab0f1e400f5d395f757
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-10-09 05:51:45 -0400

    added initial check to verify the website response is 200

--- scrapeTSNscores.py
+++ scrapeTSNscores.py
@@ -1,19 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 scoresWebsite = 'http://theapp/crsadmin/Default.aspx'
 #get the real scores
 scoresData = requests.get(scoresWebsite)
 
-print("The response from", scoresWebsite, "is", scoresData);
+print("The response from", scoresWebsite, "is", scoresData)
 print()
 #load data into bs4
 soup = BeautifulSoup(scoresData.text, 'html.parser')
 
 #get data simply by looking for each <tr>
 print("Getting data from", scoresWebsite)
-print(soup)
 
+# if the website responds with a status different than 200, the script will stop
+if (scoresData.status_code != 200):
+    print("The website returned the following response:", scoresData)
+    print("The script will stop here.")
+    sys.exit()
 
 TSNwebsite = 'https://www.tsn.ca/'
 #get the data displayed on TSN website
